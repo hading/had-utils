5f1c2938dcf8fa1941c9ffb35d9dfb495bc196d1
Howard Ding <hding2@illinois.edu>
2020-12-07 11:45:54 -0500

    Add project url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject org.clojars.hading/had-utils "0.1.0"
   :description "Personal utilites"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/hading/had-utils"
   :license {:name "Apache License, Version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0.txt"}
   :dependencies [[org.clojure/clojure "1.10.1"]]
