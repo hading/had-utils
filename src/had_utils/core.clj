ea5f14e53ce9b0e9690c4f42ff3d3a636e1ae77e
Howard Ding <hding2@illinois.edu>
2020-12-07 15:30:23 -0500

    Add map-map function to generate new map from old.

--- src/had_utils/core.clj
+++ src/had_utils/core.clj
@@ -23,3 +23,15 @@
   "Slurp a file and split into lines"
   [path]
   (str/split-lines (slurp path)))
+
+(defn map-map
+  "Construct a new map from an existing one.
+  Each of val-fn and (optional - default (fn [k v] k)) key-fn are
+  a function of two arguments, the key and value.
+  Note that to make the usage more natural the optional key-fn
+  argument goes in the second place when used."
+  ([map val-fn]
+   (map-map map val-fn (fn [k v] k)))
+  ([map key-fn val-fn]
+   (reduce-kv #(assoc %1 (key-fn %2 %3) (val-fn %2 %3)) {} map)))
+
