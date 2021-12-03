6e3436ec236e8e7ce9903219cac31a929d726e53
Howard Ding <hding2@brynmawr.edu>
2021-12-03 11:08:07 -0500

    Rename map-map to map-kv, which seems more natural.

--- src/had_utils/collections.clj
+++ src/had_utils/collections.clj
@@ -1,15 +1,17 @@
 (ns had-utils.collections)
 
-(defn map-map
+(defn map-kv
   "Construct a new map from an existing one.
-  Each of val-fn and (optional - default (fn [k v] k)) key-fn are
+  Each of val-fn and (optional - default (fn [k _] k)) key-fn are
   a function of two arguments, the key and value.
   Note that to make the usage more natural the optional key-fn
   argument goes in the second place when used."
   ([m val-fn]
-   (map-map m identity val-fn))
+   (map-map m (fn [k _] k) val-fn))
   ([m key-fn val-fn]
    (reduce-kv #(assoc %1 (key-fn %2 %3) (val-fn %2 %3)) {} m)))
+
+(def map-map map-kv)
 
 (defn count-when
   "Count the number of elements in coll where f returns true. If not supplied use identity as f."
