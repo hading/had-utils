c596572848656df8410c576324b37261ef6f6628
Howard Ding <hding2@brynmawr.edu>
2021-12-04 09:59:31 -0500

    bump version, working on deploy

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.hading/had-utils "1.0.3"
+(defproject org.clojars.hading/had-utils "1.0.4"
   :description "Personal utilites"
   :url "https://github.com/hading/had-utils"
   :license {:name "Apache License, Version 2.0"
@@ -6,4 +6,5 @@
   :dependencies [[org.clojure/clojure "1.10.1"]]
   :repl-options {:init-ns had-utils.core}
   :repositories [["releases" {:url "https://repo.clojars.org"
-                              :creds :gpg}]])
+                              :username "hading"
+                              :password [:env/lein_password :gpg]}]])
