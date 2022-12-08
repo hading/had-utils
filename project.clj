edc17af5a24a274b1cff4f1a2ef03d89eecd78a3
Howard Ding <hding2@brynmawr.edu>
2022-12-08 13:51:46 -0500

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.hading/had-utils "2.1.1"
+(defproject org.clojars.hading/had-utils "2.2.0"
   :description "Personal utilites"
   :url "https://github.com/hading/had-utils"
   :license {:name "Apache License, Version 2.0"
