ecc340a17fe2592c1243c9f02e2600e50755be71
Howard Ding <hding2@brynmawr.edu>
2023-12-03 15:01:17 -0500

    refactor

--- src/had_utils/collections.clj
+++ src/had_utils/collections.clj
@@ -78,17 +78,21 @@ one dimensional vector"
 
 (defn neighbors
   "`loc` is assumed to be a coordinate into a flattened two-dimensional grid with `cols` columens.
-  Gives a vector of coordinates of the horizontal and vertical neighbors. Does not check bounds."
-  [cols loc]
-  (mapv (partial + loc) [-1 1 cols (- cols)]))
+  Gives a vector of coordinates of the horizontal and vertical neighbors. Does not check bounds.
+  If `:with-diagonal` is true includes diagonal neighbors as well."
+  [cols loc & {:keys [:with-diagonal]}]
+  (let [offsets (if with-diagonal
+                  (let [ncols (- cols)]
+                    [(dec ncols) ncols (inc ncols) -1 1 (inc cols) cols (dec cols)])
+                  [(- cols) -1 1 cols])]
+    (mapv (partial + loc) offsets)))
 
 (defn neighbors-with-diagonals
     "`loc` is assumed to be a coordinate into a flattened two-dimensional grid with `cols` columens.
   Gives a vector of the coordinates of horizontal, vertical, and diagonal neighbors.
   Does not check bounds."
   [cols loc]
-  (let [ncols (- cols)]
-    (mapv (partial + loc) [-1 1 cols (inc cols) (dec cols) ncols (inc ncols) (dec ncols)])))
+  (neighbors cols loc :with-diagonal true))
 
 (defn multiget
   "Return a vector of the values at all `indexes` in `vec`"
