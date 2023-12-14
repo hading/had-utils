9a0d660ec5d94b704a24cf1c4a729856144c90ab
Howard Ding <hding2@brynmawr.edu>
2023-12-14 13:14:08 -0500

    Add some functions for dealing with cyclic iteration.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.hading/had-utils "3.1.0"
+(defproject org.clojars.hading/had-utils "3.2.0"
   :description "Personal utilites"
   :url "https://github.com/hading/had-utils"
   :license {:name "Apache License, Version 2.0"
--- src/had_utils/core.clj
+++ src/had_utils/core.clj
@@ -42,3 +42,45 @@ the first two arguments"
   [f]
   (fn [x y & rest]
     (apply f y x rest)))
+
+(defn find-cycle
+  "Apply `update-fn` to `input` iteratively until a cycle is found.
+  Return [`start` `end`] of the cycle, so applying `start` times is the
+  same as `end` times. An optional `hash-fn` may be provided - in this
+  case when two results have the same hash the cycle is reported."
+  ([input update-fn] (find-cycle input update-fn identity))
+  ([input update-fn hash-fn]
+   (loop [seen {(hash-fn input) 0}
+          n 1
+          input input]
+     (let [new-input (update-fn input)
+           new-key (hash-fn new-input)]
+       (if-let [n-found (get seen new-key)]
+         [n-found n]
+         (recur (assoc seen new-key n)
+                (inc n)
+                new-input))))))
+
+(defn find-cyclic-equivalent
+  "See also `find-cycle`. Assuming applying `update-fn` repeatedly to `input` produces a cycle
+  (or repeats a hash if the optional `hash-fn` is provided), finds an equivalent number of
+  applications of `update-fn` to the provided `n` but less than the number needed to reach the
+  end of the first cycle."
+  ([input n update-fn] (find-cyclic-equivalent input n update-fn identity))
+  ([input n update-fn hash-fn]
+   (let [[s e] (find-cycle input update-fn hash-fn)
+         cycle-length (- e s)]
+     (if (<= n s)
+       n
+       (+ s (mod (- n s) cycle-length))))))
+
+(defn find-cyclic
+  "See also `find-cycle`. Finds the result of applying `update-fn` to `input` `n`
+  times by first detecting a cycle in the iteration using `find-cyclic-equivalent`
+  and then computing based on that. Note that there is an inherent inefficiency
+  because we compute first to find the cycle and then again to find the answer.
+  This is necessary to support non-identity `hash-fn`."
+  ([input n update-fn] (find-cyclic input n update-fn identity))
+  ([input n update-fn hash-fn]
+   (let [n (find-cyclic-equivalent input n update-fn hash-fn)]
+     (first (drop n (iterate update-fn input))))))
