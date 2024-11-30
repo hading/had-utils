81317968fdfd293f3b691e77bfe77d0e061f5271
Howard Ding <hding2@brynmawr.edu>
2024-11-30 09:22:01 -0500

    typo

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/hading/had-utils"
   :license {:name "Apache License, Version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0.txt"}
-  :dependencies [[org.clojure/clojure "1.20.0"]
+  :dependencies [[org.clojure/clojure "1.12.0"]
                  [ubergraph "0.9.0"]]
   :plugins [[lein-codox "0.10.8"]]
   :codox {:output-path "doc"
