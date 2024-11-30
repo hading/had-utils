a2ae7edf3c13d7360b77dddb96731ef1e483c6e9
Howard Ding <hding2@brynmawr.edu>
2024-11-30 09:20:57 -0500

    update library versions; bump version

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject org.clojars.hading/had-utils "5.0.1"
+(defproject org.clojars.hading/had-utils "5.1.1"
   :description "Personal utilites"
   :url "https://github.com/hading/had-utils"
   :license {:name "Apache License, Version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0.txt"}
-  :dependencies [[org.clojure/clojure "1.11.1"]
-                 [ubergraph "0.8.2"]]
+  :dependencies [[org.clojure/clojure "1.20.0"]
+                 [ubergraph "0.9.0"]]
   :plugins [[lein-codox "0.10.8"]]
   :codox {:output-path "doc"
           :metadata {:doc/format :markdown}}
